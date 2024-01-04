87e0234939b0b6cf01258bc7132b66575ae5fde2
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-04 18:54:23 +0530

    commited change

--- src/app.js
+++ src/app.js
@@ -12,7 +12,24 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
+     try{if(value%2==0 && value>=0){
+      res.writeHead(200 , {"Content-type":"text/plain"});
+      res.end(`The number ${value} is even`);
 
+     }else if(value%2!=0 && value>=0){
+      res.writeHead(404 , {"Content-type":"text/plain"});
+      res.end(`The number ${value} is odd`);
+     }
+     else {
+      res.writeHead(400, { "Content-type": "text/plain" });
+      res.end(`Bad Request. Please provide a valid integer.`);
+    }
+  }
+     catch(error){
+      res.writeHead(400 , {"Content-type":"text/plain"});
+      res.end(`Bad Request ${error.message}`);
+     }
+    
    });
   }
 
